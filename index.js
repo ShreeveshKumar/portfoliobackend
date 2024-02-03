0884c9537d4ac95ae432a78dbec6d2fe2868087e
wait <shreeveshkr@gmail.com>
2024-02-03 18:56:48 +0530

    improved ui

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
 dotenv.config({
-    path:"./.env"
-})
+  path: "./.env",
+});
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(
-    cors({
-        origin:"https://wait-chi.vercel.app/Contact",
-    })
-)
+  cors({
+    origin: "https://shreevesh.vercel.app/Contact",
+  })
+);
 
 const User = mongoose.Schema({
-    name:String,
-    email:String,
-    message:String
-})
-
-const Handleuser = mongoose.model("message",User);
-
-
-
-require('./DB/connect');
-
-
-app.post('/Contact',async (req,res)=>{
-    const {name,email,message} = req.body;
-    const postman = await Handleuser.create({name,email,message});
-    await postman.save();
-    return postman;
+  name: String,
+  email: String,
+  message: String,
 });
 
-app.get('/',(req,res)=>{
-    res.send('hello from server');
-})
+const Handleuser = mongoose.model("message", User);
 
+require("./DB/connect");
 
+app.post("/Contact", async (req, res) => {
+  const { name, email, message } = req.body;
+  const postman = await Handleuser.create({ name, email, message });
+  await postman.save();
+  return postman;
+});
 
-app.listen(PORT)
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
+
+app.listen(PORT);
